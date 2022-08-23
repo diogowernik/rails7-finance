50053d0bc87b9a84ca969f93ab88c3b3172c0687
Diogo Wernik <11983260+diogowernik@users.noreply.github.com>
2022-08-23 12:39:31 -0300

    removed captcha from posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-      # if post.save_with_hcaptcha(response: params["h-captcha-response"], remote_ip: request.remote_ip)
+      # if @post.save_with_hcaptcha(response: params["h-captcha-response"], remote_ip: request.remote_ip)
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
